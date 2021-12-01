07efaa727242bd1de352b3cff8a253d8078ab675
Dubsky <dubcmsky@protonmail.com>
2021-12-01 19:25:23 +0100

    added JavaDoc

--- src/main/java/API/APIManager.java
+++ src/main/java/API/APIManager.java
@@ -7,6 +7,10 @@ import API.Models.Node;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Dubsky
+ * @version 1.3
+ */
 public class APIManager {
 
     private Node node;
@@ -16,14 +20,22 @@ public class APIManager {
     public APIManager() {
     }
 
-    public void sendToFrontend(int row, int column, int[] val) {
-        this.node = new Node(row, column, val);
+    /** Sends a node update to all attached frontends
+     *
+     * @param node The updated node
+     */
+    public void sendToFrontend(Node node) {
+        this.node = node;
         System.out.println("[API] Transmitting data to the frontend");
         for (IFrontend endpoint : frontends) {
             endpoint.update(this.node);
         }
     }
 
+    /** Sends a matrix structure to all backends
+     *
+     * @param matrix A two-dimensional node array to simulate a grid structure
+     */
     public void sendToBackend(Node[][] matrix) {
         System.out.println("[API] Transmitting data to the backend");
         for (IBackend endpoint : backends) {
@@ -31,20 +43,28 @@ public class APIManager {
         }
     }
 
+    /** Service layer between getting a new matrix and sending it to the backend
+     *
+     * @param matrix A two-dimensional node array to simulate a grid structure
+     */
     public void initMatrix(Node[][] matrix) {
         System.out.println("[API] Initializing a new Matrix");
         sendToBackend(matrix);
     }
 
-    public int[] getData() {
-        return node.getData();
-    }
-
+    /** Attaches a frontend to the managed list
+     *
+     * @param frontend Class with the IFrontend interface
+     */
     public void attachFrontend(IFrontend frontend) {
         frontends.add(frontend);
         System.out.println("[API] Frontend added");
     }
 
+    /** Attaches a backend to the managed list
+     *
+     * @param backend Class with the IBackend interface
+     */
     public void attachBackend(IBackend backend) {
         backends.add(backend);
         System.out.println("[API] Backend added");
