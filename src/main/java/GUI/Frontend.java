9ed595b3fdab70f0636ec9c534fcee5cfef79239
Kushurando <mnietzlo@gmail.com>
2021-12-12 18:00:40 +0100

    I hope this works

--- src/main/java/GUI/Frontend.java
+++ src/main/java/GUI/Frontend.java
@@ -3,135 +3,85 @@ package GUI;
 import API.APIManager;
 import API.Interfaces.IFrontend;
 import API.Models.Node;
-import API.Models.NodeType;
 import GUI.Generator.DepthFirst;
 import GUI.Generator.LabyrinthGenerator;
 
-import java.io.*;
+public class Frontend implements IFrontend {
 
-public class Frontend implements IFrontend{
-	
     public static final String ANSI_RESET = "\u001B[0m";
     // Declaring the background color
-    public static final String ANSI_BLACK_BACKGROUND
-        = "\u001B[40m";
-    
-    public static final String ANSI_BLUE_BACKGROUND
-    = "\u001B[44m";
-    
-    public static final String ANSI_RED_BACKGROUND
-    = "\u001B[41m";
-    
-    public static final String ANSI_YELLOW_BACKGROUND
-    = "\u001B[43m";
-    
-    public static final String ANSI_GREEN_BACKGROUND
-    = "\u001B[42m";
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
 
     private APIManager api;
     private Node[][] matrix;
 
     public Node[][] getMatrix() {
-		return matrix;
-	}
+        return matrix;
+    }
 
-	public void setMatrix(Node[][] matrix) {
-		this.matrix = matrix;
-	}
+    public void setMatrix(Node[][] matrix) {
+        this.matrix = matrix;
+    }
 
-	public Frontend(APIManager api) {
+    public Frontend(APIManager api) {
         this.api = api;
     }
 
     public void testStart() {
-    	generateLabyrinth(new DepthFirst(), 60, 30);
+        generateLabyrinth(new DepthFirst(), 60, 30);
         api.initMatrix(matrix);
     }
 
     @Override
     public void update(Node node) {
-    	
-    	
-    	int x = node.getX();
-    	int y = node.getY();
-    	
-    	matrix[x][y] = node;
-    	
-    	
+
+        int x = node.getX();
+        int y = node.getY();
+
+        matrix[x][y] = node;
+
         System.out.println("[API - Frontend] Node update received");
-		for(int i=0; i<matrix.length; i++) {
-			for(int j=0; j<matrix[0].length; j++) {
-				
-				switch(matrix[j][i].getType()) {
-				case BLOCKED:
-					System.out.print(ANSI_BLACK_BACKGROUND + "  "+ ANSI_RESET);
-					break;
-				case END:
-					System.out.print(ANSI_RED_BACKGROUND + "  "+ ANSI_RESET);
-					break;
-				case NORMAL:
-					System.out.print("  ");
-					break;
-				case PATH:
-					System.out.print(ANSI_GREEN_BACKGROUND + "  "+ ANSI_RESET);
-					break;
-				case START:
-					System.out.print(ANSI_BLUE_BACKGROUND + "  "+ ANSI_RESET);
-					break;
-				case VISITED:
-					System.out.print(ANSI_YELLOW_BACKGROUND + "  "+ ANSI_RESET);
-					break;
-				default:
-					break;
-				
-				}
-			}
-			System.out.println();
-		}
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                switch (matrix[j][i].getType()) {
+                case BLOCKED:
+                    System.out.print(ANSI_BLACK_BACKGROUND + "  " + ANSI_RESET);
+                    break;
+                case END:
+                    System.out.print(ANSI_RED_BACKGROUND + "  " + ANSI_RESET);
+                    break;
+                case NORMAL:
+                    System.out.print("  ");
+                    break;
+                case PATH:
+                    System.out.print(ANSI_GREEN_BACKGROUND + "  " + ANSI_RESET);
+                    break;
+                case START:
+                    System.out.print(ANSI_BLUE_BACKGROUND + "  " + ANSI_RESET);
+                    break;
+                case VISITED:
+                    System.out.print(ANSI_YELLOW_BACKGROUND + "  " + ANSI_RESET);
+                    break;
+                default:
+                    break;
+
+                }
+            }
+            System.out.println();
+        }
     }
-    
-    
+
     public void generateLabyrinth(LabyrinthGenerator labyrinthGenerator, int x, int y) {
-    	matrix = labyrinthGenerator.generateLabyrinth(x, y);
+        matrix = labyrinthGenerator.generateLabyrinth(x, y);
     }
-    
-    
-    
-//	public static void  main(String[]args) {
-//		Frontend f = new Frontend(null);
-//
-//		int x = 80;
-//		int y = 40;
-//		f.generateLabyrinth(new DepthFirst(), x, y);
-//		
-//	
-//		for(int i=0; i< f.getMatrix()[0].length ;i++) {
-//			for(int j=0; j<f.getMatrix().length; j++) {
-//				switch(f.getMatrix()[j][i].getType()) {
-//				case BLOCKED:
-//					System.out.print(ANSI_BLACK_BACKGROUND + "  "+ ANSI_RESET);
-//					break;
-//				case END:
-//					System.out.print(ANSI_RED_BACKGROUND + "  "+ ANSI_RESET);
-//					break;
-//				case NORMAL:
-//					System.out.print("  ");
-//					break;
-//				case PATH:
-//					System.out.print(ANSI_GREEN_BACKGROUND + "  "+ ANSI_RESET);
-//					break;
-//				case START:
-//					System.out.print(ANSI_BLUE_BACKGROUND + "  "+ ANSI_RESET);
-//					break;
-//				case VISITED:
-//					System.out.print(ANSI_YELLOW_BACKGROUND + "  "+ ANSI_RESET);
-//					break;
-//				default:
-//					break;
-//				
-//				}
-//			}
-//			System.out.println();
-//		}
-//	}
+
 }
