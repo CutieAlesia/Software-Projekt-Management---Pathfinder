72087a8c14781fac2f8ea402c8e2eecf92628b65
Kushurando <mnietzlo@gmail.com>
2021-12-12 17:29:33 +0100

    Revert "delete dupe"
    This reverts commit b08830e04c79d9962e753a5f412b99041a18bfc5.

---
+++ src/main/java/api/interfaces/IFrontend.java
@@ -0,0 +1,7 @@
+package main.java.api.interfaces;
+
+import main.java.api.models.Node;
+
+public interface IFrontend {
+    void update(Node node);
+}
---
+++ src/main/java/api/models/NodeType.java
@@ -0,0 +1,12 @@
+package main.java.api.models;
+
+/**
+ * Enum for the different node types/states
+ * 
+ * @author Finn
+ */
+public enum NodeType {
+
+	START, NORMAL, END, BLOCKED, VISITED, PATH
+	
+}
