45083add5aaa7105b1d56716d5ba3cc00a0afc44
mhertzke <maximilian.hertzke@fh-bielefeld.de>
2022-01-01 14:59:20 +0100

    Added BestFirst explanation

--- core/src/com/mygdx/labels/ExplanationLabel.java
+++ core/src/com/mygdx/labels/ExplanationLabel.java
@@ -33,12 +33,12 @@ public class ExplanationLabel extends Label {
                     "Der Algorithmus verwendet sowohl die realen Pfadkosten als auch die Schätzung der Restkosten \n(Heuristik) eines Knotens. Die Kostenfunktion lautet dabei wie folgt: f(n) = g(n) + h(n)\n" +
                     "Es wird immer ein Weg gefunden (sofern vorhanden) und dieser ist auch immer der optimale Weg\n" +
                     "Als Datenstruktur zur Speicherung der Pfade wird eine sortierte Queue (Prioritätsqueue genutzt)\n" +
-                    "Regeln für die Heuristik:\n" +
+                    "\nRegeln für die Heuristik:\n" +
                     "Damit die Optimalität und die Vollständigkeit des A* Algorithmus gewährleistet werden können, \nmüssen einige Kriterien bezüglich der Heuristik erfüllt sein.\n" +
                     "So ist die Heuristik z.B. nur dann zulässig, wenn für alle Knoten gilt, dass die Schätzung \nder Restkosten (h(n)) kleiner/gleich den tatsächlichen optimalen Restkosten eines Knotens ist.\n" +
                     "Außerdem muss die Heuristik für jeden Knoten größer/gleich 0 sein und für den Zielknoten \nexplizit 0 betragen.  \n" +
-                    "Vorgehensweise\n" +
-                    "Die Vorgehensweise des A* Algorithmus wird im Folgenden Stichpunktartig beschrieben:\n" +
+                    "\nVorgehensweise\n" +
+                    "Die Vorgehensweise des A* Algorithmus wird im Folgenden stichpunktartig beschrieben:\n" +
                     "\nSchritt 1:  Nimm den Startknoten, berechne die Gesamtkosten (bestehend aus realen Pfadkosten " +
                     "\n            und geschätzten Restkosten) für den Knoten und füge ihn in eine sortierte Queue ein. \n" +
                     "Schritt 2:  Nimm den ersten Knoten aus der Queue, kalkuliere die Gesamtkosten für alle Nachbarn " +
@@ -55,7 +55,14 @@ public class ExplanationLabel extends Label {
 
                 break;
             case BESTFIRST:
-                this.setText("Best First Best First Best First Best First Best First \n Best First Best First Best First Best First \n Best First Best First Best First Best First");
+                this.setText("Genau wie der A*- und der Branch and Bound-Algorithmus gehört auch der Best First-Algorithmus\n" +
+                    "zu den informierten Suchverfahren. Der Best First-Algorithmus setzt lediglich auf\ndie Heuristik, was bedeutet, dass die realen Pfadkosten hier \n" +
+                    "überhaupt keine Rolle spielen.\n" +
+                    "Im Gegensatz zu den A*- und Branch and Bound-Algorithmen ist der Best First-Algorithmus\n" +
+                    "weder optimal noch vollständig. Als Datenstruktur zur Speicherung der Pfade wird ebenfalls\neine sortierte Queue (Prioritätsqueue) genutzt.\n" +
+                    "\nVorgehensweise\n" +
+                    "Auch der Best First-Algorithmus verfolgt die gleiche Strategie wie der Branch and Bound-\n und der A*-Algorithmus, mit dem Unterschied, dass hier lediglich auf die Heuristik \n" +
+                    "(die Schätzung der Restkosten vom aktuellen Knoten zum Zielknoten) gesetzt wird\nund die realen Pfadkosten keine Rolle spielen.\n\n");
 
                 currentAlgoExplanation = SupportedAlgorithms.BESTFIRST;
 
